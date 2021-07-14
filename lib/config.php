89178159b68776436692c4e9bf6f3f3463eae922
yeslinpardave <81619609+yeslinpardave@users.noreply.github.com>
2021-07-14 04:04:19 -0500

    Update config.php

--- lib/config.php
+++ lib/config.php
@@ -2,9 +2,9 @@
 
 /*=============valores de la base de datos===============*/
 
-define('HOST', 'mysql-yeslin.alwaysdata.net');
-define('DB', 'yeslin_bsale_test');
-define('USER', 'yeslin_bsale_tes');
+define('HOST', 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com');
+define('DB', 'bsale_test');
+define('USER', 'bsale_test');
 define('PASSWORD', 'bsale_test');
 
 ?>
